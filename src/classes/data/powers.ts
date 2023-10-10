a335c36a464609e101357bd3a90cca201af9a2c1
Manuel Ulate <fr3zs09@gmail.com>
2023-10-10 14:52:09 -0600

    new: powers data

---
+++ src/classes/data/powers.ts
@@ -0,0 +1,24 @@
+
+interface Power {
+    id:   number;
+    desc: string;
+}
+
+export const powers:Power[] = [
+    {
+        id: 1,
+        desc: 'Power 1'
+    },
+    {
+        id: 2,
+        desc: 'Power 2'
+    },
+    {
+        id: 3,
+        desc: 'Power 3'
+    },
+    {
+        id: 4,
+        desc: 'Power 4'
+    },
+]
