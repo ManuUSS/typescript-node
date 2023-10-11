abcee38f1e1087c0faef0ef1a673d23817efabe3
Manuel Ulate <fr3zs09@gmail.com>
2023-10-11 13:37:24 -0600

    new: generics fn

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-// Importando con alias
-import { Hero as HeroClass } from "./classes/Hero";
-// Importando por defecto, aqui se puede usar otro nombre
-import powersHero from "./classes/data/powers";
 
-const Hero = "My Hero";
-
-const ironman = new HeroClass('Manu', 999, 20 );
-console.log( ironman );
-
-console.log( powersHero[1] );
 
