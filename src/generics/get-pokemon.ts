4b03a9bbfffe438a498d26607b5ed59c3b1635d1
Manuel Ulate <fr3zs09@gmail.com>
2023-10-11 14:47:51 -0600

    fix: pokemon interface return types

--- src/generics/get-pokemon.ts
+++ src/generics/get-pokemon.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import { PokemonResponse } from "../interfaces";
 
 
-export const getPoke = async ( pokemonId: number ) => {
+export const getPoke = async ( pokemonId: number ):Promise<PokemonResponse> => {
 
     const { data } = await axios.get<PokemonResponse>(`https://pokeapi.co/api/v2/pokemon/${ pokemonId }`);
-    console.log( data.name );     
+    return data;     
 
 }
