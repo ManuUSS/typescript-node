09df298c0d7077e467585b22505a5b682e01f714
Manuel Ulate <fr3zs09@gmail.com>
2023-10-11 13:59:57 -0600

    new: generics fn

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import { printObj, genericFnAny, genericFn } from "./generics/generics";
+import { Hero } from "./interfaces/hero";
 
 // Definida como any
-printObj( 123 );
-printObj( "123" );
-printObj( [ 1, 2, 3 ] );
-printObj( { a: 1, b: 2, c: 3 } );
+// printObj( 123 );
+// printObj( "123" );
+// printObj( [ 1, 2, 3 ] );
+// printObj( { a: 1, b: 2, c: 3 } );
 
 //? Definida como any, no posee intellisense
-console.log( genericFnAny( 3.141618 ).toFixed( 2 ) );
+// console.log( genericFnAny( 3.141618 ).toFixed( 2 ) );
 // console.log( genericFnAny( "3.141618" ).toFixed( 2 ) ); //! Error
 
 //? Me ayuda con el tipado y el intellisense
-console.log( genericFn( 3.141618 ).toFixed( 2 ) );
-console.log( genericFn( "3.141618" ).toUpperCase() );
+// console.log( genericFn( 3.141618 ).toFixed( 2 ) );
+// console.log( genericFn( "3.141618" ).toUpperCase() );
+
+
+const deadPool = {
+    name: 'Deadpool',
+    realName: 'Wade Winston',
+    dangerLevel: 130
+}
+
+console.log( genericFn<Hero>( deadPool ).dangerLevel );
