5927f01dae62993df5e27eb4a87e53f927cccb37
Manuel Ulate <fr3zs09@gmail.com>
2023-10-11 13:50:39 -0600

    new: generics fn

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { printObj, genericFnAny } from "./generics/generics";
+import { printObj, genericFnAny, genericFn } from "./generics/generics";
 
 // Definida como any
 printObj( 123 );
@@ -6,6 +6,10 @@ printObj( "123" );
 printObj( [ 1, 2, 3 ] );
 printObj( { a: 1, b: 2, c: 3 } );
 
-// Definida como any, no posee intellisense
+//? Definida como any, no posee intellisense
 console.log( genericFnAny( 3.141618 ).toFixed( 2 ) );
 // console.log( genericFnAny( "3.141618" ).toFixed( 2 ) ); //! Error
+
+//? Me ayuda con el tipado y el intellisense
+console.log( genericFn( 3.141618 ).toFixed( 2 ) );
+console.log( genericFn( "3.141618" ).toUpperCase() );
