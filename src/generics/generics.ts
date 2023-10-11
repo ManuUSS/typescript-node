8380042717f785a6400c81d40eb74c9dc9a7e680
Manuel Ulate <fr3zs09@gmail.com>
2023-10-11 13:44:01 -0600

    new: generics fn

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -1,4 +1,11 @@
 
+
+// A pesar de que sí, es genérica, el utilizar
+// el tipo any perdemos muchas funcionalidades del intellisense
 export const printObj = ( argument: any ) => {
     console.log( argument );
+}
+
+export function genericFn( arg: any ){
+    return arg;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { printObj } from "./generics/generics";
+import { printObj, genericFn } from "./generics/generics";
 
-
+// Definida como any
 printObj( 123 );
 printObj( "123" );
 printObj( [ 1, 2, 3 ] );
 printObj( { a: 1, b: 2, c: 3 } );
+
+// Definida como any, no posee intellisense
+console.log( genericFn( 3.141618 ).toFixed( 2 ) );
+// console.log( genericFn( "3.141618" ).toFixed( 2 ) ); //! Error
