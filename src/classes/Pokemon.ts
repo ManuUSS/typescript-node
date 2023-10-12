057ccdbf3ec896e77bd61057d635e47f5922b1fa
Manuel Ulate <fr3zs09@gmail.com>
2023-10-12 14:04:57 -0600

    new: decorators methods CheckValidPokemon

--- src/classes/Pokemon.ts
+++ src/classes/Pokemon.ts
@@ -15,6 +15,12 @@ const blockPrototype = function( constructor: Function ) {
     Object.seal( constructor.prototype );
 }
 
+function CheckValidPokemon() {
+    return function ( target: any, propertyKey: string, descriptor: PropertyDescriptor ) {
+        console.log({ target, propertyKey, descriptor });
+    }
+}
+
 @blockPrototype
 @printClgConditional( true )
 export class Pokemon {
@@ -23,6 +29,7 @@ export class Pokemon {
 
     constructor ( public name: string ) {}
 
+    @CheckValidPokemon()
     savePokemon( id: number ) {
         console.log(`Pokemon guardado ${ id }`);
     }
